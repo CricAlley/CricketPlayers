98229ae3bdc9cbe1c32ecbee9c4c215babec213a
narender <naru1790@gmail.com>
2019-05-24 21:48:31 +0530

    first drft

---
+++ CricketPlayers/Data/CricketPlayersContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CricketPlayers.Models
+{
+    public class CricketPlayersContext : DbContext
+    {
+        public CricketPlayersContext (DbContextOptions<CricketPlayersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CricketPlayers.Models.Player> Player { get; set; }
+    }
+}
---
+++ CricketPlayers/Models/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CricketPlayers.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string PlayingRole { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string BattingStyle { get; set; }
+        public string BowlingStyle { get; set; }
+        public int CricInfoId { get; set; }
+    }
+
+    public enum PlayingRole
+    {
+        TopOrderBatsman,
+        MiddleOrderBatsman,
+        Finisher,
+        Batsman,
+        Bowler,
+        DeathBowler,
+        AllRounder,
+        Wicketkeeper,
+        WicketkeeperTopOrderBatsman,
+        WicketkeeperMiddleOrderBatsman,
+        WicketkeeperFinisher
+    }
+}
