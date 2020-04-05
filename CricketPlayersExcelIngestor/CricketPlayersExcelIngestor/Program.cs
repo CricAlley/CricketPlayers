14e2845c708b4efd58382dd650d8cd14de08f3a0
narender <naru1790@gmail.com>
2020-04-05 13:02:40 +0530

    added excel ingestor

---
+++ CricketPlayersExcelIngestor/CricketPlayersExcelIngestor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CricketPlayersExcelIngestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
