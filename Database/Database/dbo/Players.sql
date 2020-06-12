56a9e48087ff4c70afaa66b6c82e1428fb4cedff
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-06-12 17:24:16 +0530

    Added Players Database

---
+++ Database/Database/dbo/Players.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Players] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [Name]             NVARCHAR (MAX) NULL,
+    [FullName]         NVARCHAR (MAX) NULL,
+    [PlayingRole]      NVARCHAR (MAX) NULL,
+    [FieldingPosition] NVARCHAR (MAX) NULL,
+    [DateOfBirth]      DATETIME       NOT NULL,
+    [BattingStyle]     NVARCHAR (MAX) NULL,
+    [BowlingStyle]     NVARCHAR (MAX) NULL,
+    [CricInfoId]       INT            NOT NULL,
+    [IsActive]         BIT            DEFAULT ((1)) NULL,
+    [CricsheetName]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Players] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
