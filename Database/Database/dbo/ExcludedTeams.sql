c687bac4ef42230242a0b48964de046f678291cd
Narender Singh Shekhawat <NShekhwat@evolenthealth.com>
2020-08-16 14:33:37 +0530

    added players alias and exluded teams

---
+++ Database/Database/dbo/ExcludedTeams.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ExcludedTeams]
+(
+	[Id]               INT            IDENTITY (1, 1) NOT NULL,    
+    [IsActive]         BIT            DEFAULT ((1)) NOT NULL,
+    [TeamName]         NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.ExcludedTeams] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
---
+++ Database/Database/dbo/PlayerAliasMapping.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PlayerAliasMapping] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,    
+    [CricInfoId]       INT            NOT NULL,
+    [IsActive]         BIT            DEFAULT ((1)) NOT NULL,
+    [CricsheetName]    NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_dbo.PlayerAliasMapping] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
