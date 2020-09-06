96125c22d8713726a7298266fdc24abf638b8542
narender <naru1790@gmail.com>
2020-09-06 14:08:01 +0530

    as

--- Database/Database/publishDbSchema.sh
+++ Database/Database/publishDbSchema.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sqlpackage /a:Publish /tcs:"$CONNECTIONSTRINGS_CRICKETDB" /sf:Database.dacpac
+sqlpackage /a:Publish /tcs:$CONNECTIONSTRINGS_CRICKETDB /sf:Database.dacpac
 sqlpackage /a:Publish /tcs:"$(CONNECTIONSTRINGS_CRICKETDB)" /sf:Database.dacpac
 sqlpackage /a:Publish /tcs:$(CONNECTIONSTRINGS_CRICKETDB) /sf:Database.dacpac
