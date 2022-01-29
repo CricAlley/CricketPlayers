b49e8589053bf0936a809c41b71c62bd7980a030
naru1 <naru1@LAPTOP-3AQTASAE>
2022-01-29 19:35:56 +0530

    added cricRegister

---
+++ Database/Database/dbo/playerRegister.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[playerRegister]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Identifier] VARCHAR(100) NOT NULL, 
+    [UniqueName] VARCHAR(100) NOT NULL, 
+    [Names] VARCHAR(1000) NULL, 
+    [CricInfoId] INT NOT NULL, 
+    [IsActive] BIT NOT NULL DEFAULT 0
+)
